3d14485344188f5605d26042abe23b71c3d13bbb
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-03 16:50:37 +0530

    get project name method added

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -1,15 +1,21 @@
 error() {
-  local c="2,999 s/^/ !     /"
+	local c="2,999 s/^/ !     /"
 	# send all of our output to stderr
 	exec 1>&2
 
 	echo -e "\033[1;31m" # bold; red
 	echo -n " !     ERROR: "
 	# this will be fed from stdin
-  case $(uname) in
+	case $(uname) in
 		Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
 		*)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
 	esac
 	echo -e "\033[0m" # reset style
 	exit 1
+}
+
+getprojectname() {
+	local projectfile=$(x=$(dirname $(find $1 -maxdepth 1 -iname Startup.cs | head -1)); while [[ "$x" =~ $1 ]] ; do find "$x" -maxdepth 1 -name *.csproj; x=`dirname "$x"`; done)
+	local projectname=$(basename ${projectfile%.*})
+	return projectname;
 }
