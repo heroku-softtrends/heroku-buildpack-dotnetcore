aba095273a55b08525a83a5f16fb015a2e0370bf
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-03 13:55:50 +0530

    common methods defined

---
+++ bin/util/common.sh
@@ -0,0 +1,15 @@
+error() {
+  local c="2,999 s/^/ !     /"
+	# send all of our output to stderr
+	exec 1>&2
+
+	echo -e "\033[1;31m" # bold; red
+	echo -n " !     ERROR: "
+	# this will be fed from stdin
+  case $(uname) in
+		Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+		*)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+	esac
+	echo -e "\033[0m" # reset style
+	exit 1
+}
