12e37b67ebf5a7ef095013030f1528ff12f48ab0
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-03 17:50:41 +0530

    Update common.sh

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -16,7 +16,10 @@ error() {
 
 getprojectname() {
 	local projectfile=$(x=$(dirname $(find $1 -maxdepth 1 -type f | head -1)); while [[ "$x" =~ $1 ]] ; do find "$x" -maxdepth 1 -name *.csproj; x=`dirname "$x"`; done)
-	echo $projectfile
-	projectname=$(basename ${projectfile%.*})
+	if [[ $projectfile ]]; then
+		projectname=$(basename ${projectfile%.*})
+	else
+		projectname=""
+	fi
 	echo $projectname
 }
