c2f3f291d822955d53a6a2fc1645547d76cc8674
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-04 16:56:01 +0530

    Update common.sh

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -49,10 +49,12 @@ function export_env_dir() {
   	local env_dir=$1
   	local whitelist_regex=${2:-''}
   	local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+	print "env dir: $env_dir"
   	if [ -d "$env_dir" ]; then
     		for e in $(ls $env_dir); do
       		echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
       		export "$e=$(cat $env_dir/$e)"
+		print "env var: $e"
       		:
     		done
   	fi
