517a3134a2c44d921ab0ce0f29d708a76f6e772b
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-07 11:41:09 +0530

    Delete aspnetcore-defaults.sh

--- profile/aspnetcore-defaults.sh
+++
@@ -1,7 +0,0 @@
-export PATH="$PATH:$HOME/.heroku/dotnet:$HOME/.apt/usr/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib"
-export LIBRARY_PATH="$LIBRARY_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib"
-export INCLUDE_PATH="$INCLUDE_PATH:$HOME/.apt/usr/include"
-export CPATH="$INCLUDE_PATH"
-export CPPPATH="$INCLUDE_PATH"
-export APT_CONFIG_PATH="$APT_CONFIG_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu/aptconfig:$HOME/.apt/usr/lib/i386-linux-gnu/aptconfig:$HOME/.apt/usr/lib/aptconfig"
