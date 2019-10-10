ca2b798ce864181e811ef841787e5c97b9e1c4bd
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-10 12:58:09 +0530

    Update install.sh

--- bin/util/install.sh
+++ bin/util/install.sh
@@ -98,7 +98,7 @@ function apt_install(){
     local deb="$(ls $apt_cache_dir/archives | grep $package)"
     dpkg -x $apt_cache_dir/archives/$deb "$BUILD_DIR/.apt/"
     ls -a "$BUILD_DIR/.apt/"
-    dpkg -c $package
+    dpkg -c $apt_cache_dir/archives/$deb
     dpkg -l $package
     #ln -s $BUILD_DIR/.apt/$package-@PACKAGE_VERSION@ ${DESTDIR}/usr/local/bin/s
   done
