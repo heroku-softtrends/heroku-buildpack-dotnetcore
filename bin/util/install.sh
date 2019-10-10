8eba4f531a528a1f70cf9db1a562824910d80051
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-10 13:06:08 +0530

    Update install.sh

--- bin/util/install.sh
+++ bin/util/install.sh
@@ -99,6 +99,7 @@ function apt_install(){
     dpkg -x $apt_cache_dir/archives/$deb "$BUILD_DIR/.apt/"
     ls -a "$BUILD_DIR/.apt/"
     dpkg -c $apt_cache_dir/archives/$deb
+    ls -a $BUILD_DIR/.apt/usr/share/doc/"
     dpkg -l $package
     #ln -s $BUILD_DIR/.apt/$package-@PACKAGE_VERSION@ ${DESTDIR}/usr/local/bin/s
   done
