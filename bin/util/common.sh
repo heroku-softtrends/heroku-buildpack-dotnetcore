dbf1759a5d14e21e4ee6e7093550df9e60aaed1d
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-10 15:42:22 +0530

    get_framework_version updated

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -56,21 +56,21 @@ error() {
 }
 
 export_env_dir() {
-  local env_dir=$1
-  if [ -d "$env_dir" ]; then
-    local whitelist_regex=${2:-''}
-    local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG|BUILD_DIR)$'}
-    # shellcheck disable=SC2164
-    pushd "$env_dir" >/dev/null
-    for e in *; do
-      [ -e "$e" ] || continue
-      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
-      export "$e=$(cat "$e")"
-      :
-    done
-    # shellcheck disable=SC2164
-    popd >/dev/null
-  fi
+	local env_dir=$1
+	if [ -d "$env_dir" ]; then
+		local whitelist_regex=${2:-''}
+		local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG|BUILD_DIR)$'}
+		# shellcheck disable=SC2164
+		pushd "$env_dir" >/dev/null
+		for e in *; do
+			[ -e "$e" ] || continue
+			echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+			export "$e=$(cat "$e")"
+			:
+		done
+		# shellcheck disable=SC2164
+		popd >/dev/null
+	fi
 }
 
 get_project_file() {
@@ -87,9 +87,9 @@ get_project_name() {
 	echo $projectname
 }
 
-get_project_version() {
+get_framework_version() {
 	local projectversion=$(grep -oPm1 "(?<=<TargetFramework>)[^<]+" $1/*.csproj)
-	echo $projectversion
+	echo "$(projectversion//[a-z,.]/)"
 }
 
 get_netcore_version() {
