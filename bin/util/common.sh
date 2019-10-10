3bdf713f7b20c3b481104bbcb0c61e7b674b343b
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-10 18:09:14 +0530

    Update common.sh

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -90,9 +90,9 @@ get_project_name() {
 get_framework_version() {
 	local target_framework=$(grep -oPm1 "(?<=<TargetFramework>)[^<]+" $1/*.csproj)
 	if [[ $target_framework =~ ";" ]]; then
-	 	echo $(cut -d ';' -f 1 <<< $target_framework)
+	 	echo $(cut -d ';' -f 1 <<< $target_framework) + 0
 	else
-		echo ${target_framework//[a-z]/}
+		echo ${target_framework//[a-z]/} + 0
 	fi
 }
 
