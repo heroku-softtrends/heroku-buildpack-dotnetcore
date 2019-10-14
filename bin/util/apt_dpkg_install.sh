f59e82135911a102be6f0f785f0725206817690f
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-14 17:38:10 +0530

    Update apt_dpkg_install.sh

--- bin/util/apt_dpkg_install.sh
+++ bin/util/apt_dpkg_install.sh
@@ -32,7 +32,7 @@ function apt_install(){
 			print "Fetching .debs for $package"
 			apt-get $apt_options -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -d install --reinstall $package | indent
 		fi
-		dpkg --info $package
+		dpkg --info $apt_cache_dir/archives/$(basename $package .deb).deb
 	done
 	
 	print "Downloaded package: $is_pakage_downloaded"
