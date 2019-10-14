9aeb17fe7d5502fa5c485d86fcac73fe201c7f28
Softtrends Cloud Development Practice <heroku@softtrends.com>
2019-10-14 18:15:22 +0530

    Update apt_dpkg_install.sh

--- bin/util/apt_dpkg_install.sh
+++ bin/util/apt_dpkg_install.sh
@@ -32,7 +32,7 @@ function apt_install(){
 			is_installed=$(is_dpkg_installed $package)
 		fi
 		
-		echo "ppp: $is_installed == 0"
+		echo "ppp: $is_installed == 1"
 		
 		if [[ $is_installed == 0 ]]; then
 			if [[ $package == *deb ]]; then
